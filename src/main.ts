1ecf4fda04ae2c4e8048cefb7287c29200a935fc
ErikoX <ponotone@gmail.com>
2022-09-27 16:21:48 +0200

    Edit template to have typescript enabled

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import kaboom from "kaboom";
 
-kaboom({
+const k = kaboom({
     width: 1280,
     height: 720,
-    canvas: document.getElementById("game"),
     background: [100,100,200],
     font: "sink",
     debug: true,
