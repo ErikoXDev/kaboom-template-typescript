29a0e91cd8788e6af944099d05e52d82a9291d2b
ErikoX <ponotone@gmail.com>
2022-09-27 19:02:25 +0200

    -

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs")
-var files = []
-fs.readdir("src", (err, files) => {
-    files.forEach(file => {
-        if (file.endsWith(".ts")) files.push("src/" + file)
-    })
-})
 
-require('esbuild').build({
-    entryPoints: files,
-    bundle: true,
-    keepNames: true,
-    target: "es6",
-    outfile: 'dist/dist.js',
-    watch: true
-}).catch(() => process.exit(1))
+
 
 const app = express();
 
